0b0f313262a1a0d3414b646987f574307d41183c
Aryaman Singh <aryaman3007@gmail.com>
2023-05-11 11:34:49 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+        <div className='container py-5'>
+            <h1>About Todo-List</h1>
+            <p> 
+            Welcome to our todo list app! Our app is designed to help you organize your tasks and stay on top of your to-do list.
+            With our app, you can easily create tasks, set due dates and reminders, and categorize your tasks by project or priority.
+            You can also track your progress and mark tasks as completed. Our app is designed with simplicity and usability in mind, 
+            so you can quickly and easily manage your tasks without any hassle. You can access your tasks from anywhere, whether you're 
+            at your desk or on the go. Whether you're managing your personal to-do list or working on a team project, our app is the perfect 
+            tool to help you stay organized and productive. Try it out today and start crossing off those tasks!
+            </p>
         </div>
     )
 }
